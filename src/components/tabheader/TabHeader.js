fd840e0357488bab53d8b41798d642fab007e26a
VadimPonomarov <pvs.versia@gmail.com>
2022-02-01 22:41:38 +0200

    Last commit

--- src/components/tabheader/TabHeader.js
+++ src/components/tabheader/TabHeader.js
@@ -34,10 +34,10 @@ function TabHeader() {
             <h5 className={'col-2 border p-2 text-center align-items-center d-flex justify-content-center'}>Model</h5>
             <h5 className={'col-2 border p-2 text-center align-items-center d-flex justify-content-center'}>Price</h5>
             <h5 className={'col-2 border p-2 text-center align-items-center d-flex justify-content-center'}>Year</h5>
-            <div className={'col-2  d-flex flex-wrap align-items-center justify-content-center'}>
-                <div className={'col-12 text-center'}>Server-side operations</div>
+            <div className={'col-2  d-flex flex-wrap align-items-center justify-content-center border mb-2'}>
+                <div className={'col-12 text-center'}><b className={'text-uppercase text-primary'}>Async operations</b></div>
                 <button
-                    type={'button'}
+                    type={'button btn-secondary btn'}
                     className={'btn btn-sm border col-3 m-1 btn-outline-danger p-1'}
                     onClick={() => handleClick('push')}
                     style={{height: '30px'}}
@@ -45,7 +45,7 @@ function TabHeader() {
                     <p>Push</p>
                 </button>
                 <button
-                    type={'button'}
+                    type={'button btn-secondary btn'}
                     className={'btn btn-sm border col-3 m-1 btn-outline-danger p-1'}
                     onClick={() => handleClick('delete')}
                     style={{height: '30px'}}
@@ -53,7 +53,7 @@ function TabHeader() {
                     <p>Delete</p>
                 </button>
                 <button
-                    type={'button'}
+                    type={'button btn-secondary btn'}
                     className={'btn btn-sm border col-3 m-1 btn-outline-danger p-1'}
                     onClick={() => handleClick('getAll')}
                     style={{height: '30px'}}
